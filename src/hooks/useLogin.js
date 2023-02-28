f16643dde8a267b8226a86ff7b2057974289889b
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-02-28 21:59:22 +0600

    Login functionality added

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setLoading(true);
+    setError(null);
+
+    const res = await fetch("http://localhost:5000/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const json = await res.json();
+
+    // res.ok === false
+    if (!res.ok) {
+      setLoading(false);
+      setError(json.error);
+    }
+
+    if (res.ok) {
+      //Update auth context
+      dispatch({ type: "LOGIN", payload: json });
+
+      //Save user in to the local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      setLoading(false);
+    }
+  };
+
+  return { login, error, loading };
+};
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,24 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { login, error, loading } = useLogin();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    //Login user
+    await login(email, password);
+  };
+
   return (
-    <form className='login-form flex flex-col gap-5 py-20 max-w-sm mx-auto'>
+    <form
+      onSubmit={handleLogin}
+      className='login-form flex flex-col gap-5 py-20 max-w-sm mx-auto'
+    >
       <h2 className='text-4xl font-medium text-sky-400 mb-10'>Login</h2>
 
       <div className='form-ctrl flex flex-col gap-2'>
@@ -41,11 +54,18 @@ const Login = () => {
         />
       </div>
       <button
+        disabled={loading}
         type='submit'
         className='bg-sky-400 py-3 text-slate-900 rounded-md mt-3 hover:bg-sky-500 duration-300'
       >
         Log in
       </button>
+
+      {error && (
+        <p className='bg-rose-500/20 rounded-lg p-5 text-rose-500 border border-rose-500'>
+          {error}
+        </p>
+      )}
     </form>
   );
 };
